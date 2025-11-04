c0853f588e2f05872c00a7815e7bd1b824acdd5f
Suleman Babar <sultansuleman892@gmail.com>
2025-11-04 10:50:16 +0500

    updated

--- app/components/users/mobile-sidebar.jsx
+++ app/components/users/mobile-sidebar.jsx
@@ -14,16 +14,14 @@ const MobileSidebar = ({ setBreadcrumbsData, toggleSidebar, isOpen }) => {
     <div>
         <div
       className={`
-        fixed top-0 left-0 z-10 h-full w-[170px] 
+        fixed top-0 left-0 z-50 h-full w-[170px] 
         bg-[#800080] text-white px-3
         transform transition-transform duration-700 ease-in-out
         ${isOpen ? "translate-x-0" : "-translate-x-full"}
         md:hidden
       `}
     >
-          <div
-            className="pl-5 pt-3 transition-all duration-500 ease-in-out  px-3  h-full bg-[#800080] text-white box-border flex flex-wrap flex-col"
-          >
+          <div className="flex flex-col h-full bg-[#800080] text-white overflow-y-auto">
             <div className="flex items-center justify-between">
               <h1 className="flex items-center gap-3 bg-white p-2 rounded-sm text-[#A80080] text-[12px] font-bold my-[15px] ">
                 <FaUserFriends className="text-[25px]" />
